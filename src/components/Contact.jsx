59232f3a2e81bb989f9e7f3053afefb86682ead5
stephen-ics <stephenni1234@gmail.com>
2024-05-14 01:18:24 -0400

    configured email js in the contact form!

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,11 +18,40 @@ const Contact = () => {
   const [loading, setLoading] = useState(false)
 
   const handleChange = (e) => {
+    const { name, value } = e.target;
 
+    setForm({ ...form, [name]: value })
   }
 
   const handleSubmit = (e) => {
+    e.preventDefault();
 
+    setLoading(true)
+    emailjs.send(
+      'service_id', 
+      'template_id', 
+      {
+        from_name: form.name,
+        to_name: 'Stephen',
+        from_email: form.email,
+        to_email: 'stephenni1234@gmail.com',
+        message: form.message,
+      },
+      'public_key'
+    ).then(() => {
+      setLoading(false);
+      alert("Thank you! I will get back to you as soon as possible! :)")
+
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      })
+    }, (error) => {
+      setLoading(false)
+      console.log(error)
+      alert("Something went wrong")
+    })
   }
 
   return (
